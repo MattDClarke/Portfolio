fcaad63f3e23f7fdae0f001cdf7e2ecf1be07618
Matthew Clarke <m_d_clarke@hotmail.com>
2021-12-16 13:59:48 +0200

    added Sanity backend

---
+++ Sanity/README.md
@@ -0,0 +1,7 @@
+# Backend CMS
+
+## Development
+
+- Initialize an app (create a project): `sanity init --reconfigure`
+- Log in from the commandline: `sanity login`
+- Run the app: `npm start` -> runs `sanity start`. Runs localhost server (Sanity Studio). Sanity is the API, Sanity Studio is the UI to view and update data.
---
+++ Sanity/schemas/Project.js
@@ -0,0 +1,31 @@
+import { GrNotes as icon } from 'react-icons/gr';
+
+export default {
+  name: 'project',
+  // visible title
+  title: 'Projects',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Project Name',
+      type: 'string',
+      description: 'Name of the project',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'imageHomePage',
+      title: 'Image for home page',
+      type: 'image',
+    },
+  ],
+};
